7e744be332b186476112af27ba4d54854c0659f1
Martijn van der Linden <martijn.van.der.linden@amsterdam.nl>
2026-03-22 12:16:33 +0100

    feat(ngrok); enable ngrok public URL to make PowerApps accessible to localhost

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+         allowedHosts: [
+                'smokelike-ciera-noncorpuscular.ngrok-free.dev',
+                'localhost',
+                '127.0.0.1',
+              ],
       },
       plugins: [react()],
       define: {
